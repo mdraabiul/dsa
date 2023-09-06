70fd6862afb618e21472bc63d597cc0dab689c87
rabiul <mdrabiul.asia@gmail.com>
2023-09-06 21:34:05 +0600

    Decimal to Binary function added

--- index.cpp
+++ index.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+int decimalToBinary()
+{
+    int n;
+    cout << "Type the value of n? " << endl;
+    cin >> n;
 
+    int i = 0;
+    int ans = 0;
+
+    while (n != 0)
+    {
+        int bit = n & 1;
+        ans = bit * pow(10, i) + ans;
+
+        n = n >> 1;
+        i++;
+    }
+    cout << ans << endl;
+}
 
 int main()
 {
-    cout << "hello world" << endl;
-
-    return 0;
+    decimalToBinary();
 }
